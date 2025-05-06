d52ad5bb628f5359ac47db08e7032d7b41d82293
Mark Paul <markpaulbusiness66@gmail.com>
2025-05-06 10:35:46 +0300

    completed store setup for blogs

--- src/app/blogs/blogs.store.ts
+++ src/app/blogs/blogs.store.ts
@@ -1,1 +1,36 @@
-import {signalStore, withMethods} from "@ngrx/signals"
+import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
+import { BlogInterface } from "./blogs.interface";
+import { BlogsService } from "./blogs.service";
+import { inject } from "@angular/core";
+import { pipe, switchMap } from "rxjs";
+import { tapResponse } from "@ngrx/operators";
+import { rxMethod } from "@ngrx/signals/rxjs-interop";
+
+type BlogState = {
+  collection: BlogInterface[];
+};
+
+export const BlogStore = signalStore(
+  withState<BlogState>({
+    collection: [],
+  }),
+  withMethods((store) => {
+    const blogService = inject(BlogsService);
+    return {
+      getBlogs: rxMethod<void>(
+        pipe(
+          switchMap(() => {
+            return blogService.getBlogs().pipe(
+              tapResponse({
+                next: (blogs) => {
+                  patchState(store, { collection: blogs });
+                },
+                error: () => {},
+              }),
+            );
+          }),
+        ),
+      ),
+    };
+  }),
+);
